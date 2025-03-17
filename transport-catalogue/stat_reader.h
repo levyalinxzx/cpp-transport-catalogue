c53cf9d9752fd0d127afe2ea94727a27e7986a7c
levyalinxzx <nufuha9@gmail.com>
2025-03-17 10:21:10 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,12 @@
-// место для вашего кода
+#pragma once
+
+#include <iosfwd>
+#include <string_view>
+
+#include "transport_catalogue.h"
+namespace transport_catalogue{
+namespace detail {
+void ParseAndPrintStat(const TransportCatalogue& tansport_catalogue, std::string_view request,
+                       std::ostream& output);
+}//namespace detail
+}//namespace transport_catalogue
