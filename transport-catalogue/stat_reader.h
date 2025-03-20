7e5eace1fb4967d42f21d80d03b91817ba7d393f
levyalinxzx <nufuha9@gmail.com>
2025-03-20 10:58:10 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -6,7 +6,10 @@
 #include "transport_catalogue.h"
 namespace transport_catalogue{
 namespace detail {
-void ParseAndPrintStat(const TransportCatalogue& tansport_catalogue, std::string_view request,
-                       std::ostream& output);
+void PrintBus(const TransportCatalogue& transport_catalogue, std::string_view request, std::ostream& output);
+void PrintStop(const TransportCatalogue& transport_catalogue, std::string_view request, std::ostream& output);
+void ParseAndPrintStat(const TransportCatalogue& tansport_catalogue, std::string_view request, std::ostream& output);
+
+
 }//namespace detail
 }//namespace transport_catalogue
