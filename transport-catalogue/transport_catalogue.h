e9ada94cc02f1f9e15af2b6c59e559c9a960d2ed
levyalinxzx <nufuha9@gmail.com>
2025-04-01 09:29:13 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -23,17 +23,10 @@ struct Bus {
 	std::vector<const Stop*> stops;	
 };
 
-struct Distance{
-    const Stop* A;
-    const Stop* B;
-    int length;
-};
-
-
 struct BusInfo {
     size_t size;
     int unique_stops;
-    size_t length_route;
+    int length_route;
     double curvature;
 };
 
@@ -51,12 +44,11 @@ class TransportCatalogue {
 public:
     void AddStop(const Stop& stop);
     void AddBus(const Bus& bus);
-    void AddDistance(const std::vector<Distance>& distances);
+    void SetDistance(const Stop* first, const Stop* second, int distance);
     const Stop* GetStop(std::string_view stop) const;
     const Bus* GetBus(std::string_view bus) const ;
     BusInfo GetBusInfo(const Bus* bus) const;
     const std::unordered_set<const Bus*>& GetBusesToStop(const Stop* stop) const; 
-    size_t GetDistanceToBus(const Bus* bus) const;
 private:
     std::deque<Stop> stops_;
     std::deque<Bus> buses_;
@@ -68,6 +60,7 @@ private:
     size_t GetNumberOfStops(const Bus* bus) const;
     int GetUniqueStops(const Bus* bus) const;
     double GetLengthRoute(const Bus* bus) const;
-    size_t GetDistance(const Stop* first, const Stop* second) const;
+    int GetDistance(const Stop* first, const Stop* second) const;
+    int GetDistanceToBus(const Bus* bus) const;
 };
 }//namespace transport_catalogue
