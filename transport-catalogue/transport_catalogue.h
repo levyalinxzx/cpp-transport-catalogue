e19d52143e7d95a69f221acd7339fe9840946fb7
levyalinxzx <nufuha9@gmail.com>
2025-04-13 19:13:23 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -32,11 +32,7 @@ public:
     const Stop* GetStop(std::string_view stop) const;
     const Bus* GetBus(std::string_view bus) const; 
     const std::deque<Bus>& GetSortedAllBuses() const;
-    //GetBusInfo я перенес в request_handler.cpp
-    size_t GetNumberOfStops(const Bus* bus) const;
-    int GetUniqueStops(const Bus* bus) const;
-    double GetLengthRoute(const Bus* bus) const;
-    int GetDistanceToBus(const Bus* bus) const;
+    const BusInfo GetBusInfo(std::string_view bus_name) const;
     const std::unordered_set<const Bus*>& GetBusesToStop(const Stop* stop) const;
 private:
     std::deque<Stop> stops_;
@@ -47,5 +43,9 @@ private:
     std::unordered_map<std::pair<const Stop*, const Stop*>, int, DistanceHasher> distance_to_stop;
 
     int GetDistance(const Stop* first, const Stop* second) const;
+    size_t GetNumberOfStops(const Bus* bus) const;
+    int GetUniqueStops(const Bus* bus) const;
+    double GetLengthRoute(const Bus* bus) const;
+    int GetDistanceToBus(const Bus* bus) const;
 };
 }//namespace transport_catalogue
