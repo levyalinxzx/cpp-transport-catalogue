ca56523344f3bf0cb10f482e9e8d1e7927705347
levyalinxzx <nufuha9@gmail.com>
2025-04-13 19:15:00 +0300

    Update request_handler.h

--- transport-catalogue/request_handler.h
+++ transport-catalogue/request_handler.h
@@ -1,25 +1,24 @@
-
 #pragma once
 
 #include "json.h"
 #include "transport_catalogue.h"
 #include "map_renderer.h"
  
- class RequestHandler {
- public:
-    RequestHandler(const transport_catalogue::TransportCatalogue& catalogue, const renderer::MapRenderer& renderer)
+class RequestHandler {
+public:
+    explicit RequestHandler(transport_catalogue::TransportCatalogue& catalogue, const renderer::MapRenderer& renderer)
         : renderer_(renderer)
         , catalogue_(catalogue)
-    {}
-
+    {
+    }
     std::vector<std::string> GetSortedBusesToStop(std::string_view stop_name) const ;
-    transport_catalogue::BusInfo GetBusInfo(std::string_view bus_name) const;
     bool IsBusNumber(const std::string_view bus_number) const ;
     bool IsStopName(const std::string_view stop_name) const;
+    const transport_catalogue::BusInfo GetBusStat(std::string_view bus_name) const;
     svg::Document RenderMap() const;
      
     
- private:
+private:
     const renderer::MapRenderer& renderer_;
-    const transport_catalogue::TransportCatalogue catalogue_;
- };
+    const transport_catalogue::TransportCatalogue& catalogue_;   
+};
