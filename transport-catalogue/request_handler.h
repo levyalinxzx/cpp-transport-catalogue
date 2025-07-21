248396a5b35aab2417ef420012c1d9c0bbe70a55
levyalinxzx <nufuha9@gmail.com>
2025-07-21 14:59:16 +0300

    Update request_handler.h

--- transport-catalogue/request_handler.h
+++ transport-catalogue/request_handler.h
@@ -3,22 +3,27 @@
 #include "json.h"
 #include "transport_catalogue.h"
 #include "map_renderer.h"
+#include "transport_router.h"
  
 class RequestHandler {
 public:
-    explicit RequestHandler(transport_catalogue::TransportCatalogue& catalogue, const renderer::MapRenderer& renderer)
+    explicit RequestHandler(tc::TransportCatalogue& catalogue, const renderer::MapRenderer& renderer, const tc::router::TransportRouter& router)
         : renderer_(renderer)
         , catalogue_(catalogue)
+        , router_(router)
     {
     }
-    std::vector<std::string> GetSortedBusesToStop(std::string_view stop_name) const ;
+    const tc::Buses* GetBusesToStop(std::string_view stop_name) const ;
     bool IsBusNumber(const std::string_view bus_number) const ;
     bool IsStopName(const std::string_view stop_name) const;
-    const transport_catalogue::BusInfo GetBusStat(std::string_view bus_name) const;
+    const tc::BusInfo GetBusStat(std::string_view bus_name) const;
+    std::optional<tc::router::RouteInfo> FindRoute(std::string_view stop_name_from, std::string_view stop_name_to) const;
+    
     svg::Document RenderMap() const;
      
     
 private:
     const renderer::MapRenderer& renderer_;
-    const transport_catalogue::TransportCatalogue& catalogue_;   
+    const tc::TransportCatalogue& catalogue_;
+    const tc::router::TransportRouter& router_;
 };
