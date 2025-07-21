b43e416d38acd2e0a72edad0ac6a270b49af2c86
levyalinxzx <nufuha9@gmail.com>
2025-07-21 14:57:41 +0300

    Update map_renderer.cpp

--- transport-catalogue/map_renderer.cpp
+++ transport-catalogue/map_renderer.cpp
@@ -1,3 +1,4 @@
+
 #include "map_renderer.h"
 
 
@@ -7,15 +8,15 @@ bool IsZero(double value) {
      return std::abs(value) < EPSILON;
 }
  
-std::vector<svg::Polyline> MapRenderer::GetRouteLines(const std::deque<transport_catalogue::Bus>& buses, const SphereProjector& sp) const {
+std::vector<svg::Polyline> MapRenderer::GetRouteLines(const std::deque<tc::Bus>& buses, const SphereProjector& sp) const {
     std::vector<svg::Polyline> result;
     size_t color_num = 0;
     for (const auto& bus: buses) {
-        if (bus.stops.empty()) {
+        if (bus.stops_.empty()) {
             continue;
         }
         svg::Polyline line;
-        for (const auto& stop : bus.stops) {
+        for (const auto& stop : bus.stops_) {
             line.AddPoint(sp(stop->coordinates_));
         }
         line.SetStrokeColor(render_settings_.color_palette[color_num]);
@@ -24,8 +25,12 @@ std::vector<svg::Polyline> MapRenderer::GetRouteLines(const std::deque<transport
         line.SetStrokeLineCap(svg::StrokeLineCap::ROUND);
         line.SetStrokeLineJoin(svg::StrokeLineJoin::ROUND);
          
-        if (color_num < (render_settings_.color_palette.size() - 1)) ++color_num;
-        else color_num = 0;
+        if (color_num < (render_settings_.color_palette.size() - 1)) {
+            ++color_num;
+        }
+        else {
+            color_num = 0;
+        }
          
         result.push_back(line);
     }
@@ -33,21 +38,21 @@ std::vector<svg::Polyline> MapRenderer::GetRouteLines(const std::deque<transport
     return result;
 }
 
- std::vector<svg::Text> MapRenderer::GetBusLabel(const std::deque<transport_catalogue::Bus>& buses, const SphereProjector& sp) const {
+ std::vector<svg::Text> MapRenderer::GetBusLabel(const std::deque<tc::Bus>& buses, const SphereProjector& sp) const {
     std::vector<svg::Text> result;
     size_t color_num = 0;
     for (const auto& bus : buses) {
-        if (bus.stops.empty()) {
+        if (bus.stops_.empty()) {
             continue;
         }
         svg::Text text;
         svg::Text underlayer;
-        text.SetPosition(sp(bus.stops[0]->coordinates_));
+        text.SetPosition(sp(bus.stops_[0]->coordinates_));
         text.SetOffset(render_settings_.bus_label_offset);
         text.SetFontSize(render_settings_.bus_label_font_size);
         text.SetFontFamily("Verdana");
         text.SetFontWeight("bold");
-        text.SetData(bus.id);
+        text.SetData(bus.name_);
         text.SetFillColor(render_settings_.color_palette[color_num]);
         if (color_num < (render_settings_.color_palette.size() - 1)) {
             ++color_num;
@@ -55,12 +60,12 @@ std::vector<svg::Polyline> MapRenderer::GetRouteLines(const std::deque<transport
         else {
             color_num = 0;
         }
-        underlayer.SetPosition(sp(bus.stops[0]->coordinates_));
+        underlayer.SetPosition(sp(bus.stops_[0]->coordinates_));
         underlayer.SetOffset(render_settings_.bus_label_offset);
         underlayer.SetFontSize(render_settings_.bus_label_font_size);
         underlayer.SetFontFamily("Verdana");
         underlayer.SetFontWeight("bold");
-        underlayer.SetData(bus.id);
+        underlayer.SetData(bus.name_);
         underlayer.SetFillColor(render_settings_.underlayer_color);
         underlayer.SetStrokeColor(render_settings_.underlayer_color);
         underlayer.SetStrokeWidth(render_settings_.underlayer_width);
@@ -70,11 +75,11 @@ std::vector<svg::Polyline> MapRenderer::GetRouteLines(const std::deque<transport
         result.push_back(underlayer);
         result.push_back(text);
         
-        if (!(bus.is_circle) && bus.stops[0] != bus.stops[bus.stops.size()/2]) {
+        if (!bus.is_circle_ && bus.stops_[0] != bus.stops_[bus.stops_.size()/2]) {
             svg::Text text2 {text};
             svg::Text underlayer2 {underlayer};
-            text2.SetPosition(sp(bus.stops[bus.stops.size()/2]->coordinates_));
-            underlayer2.SetPosition(sp(bus.stops[bus.stops.size()/2]->coordinates_));
+            text2.SetPosition(sp(bus.stops_[bus.stops_.size()/2]->coordinates_));
+            underlayer2.SetPosition(sp(bus.stops_[bus.stops_.size()/2]->coordinates_));
             
             result.push_back(underlayer2);
             result.push_back(text2);
@@ -84,7 +89,7 @@ std::vector<svg::Polyline> MapRenderer::GetRouteLines(const std::deque<transport
     return result;
 }
 
-std::vector<svg::Circle> MapRenderer::GetStopsSymbols(std::map<std::string_view, const transport_catalogue::Stop*> stops, const SphereProjector& sp) const {
+std::vector<svg::Circle> MapRenderer::GetStopsSymbols(std::map<std::string_view, const tc::Stop*> stops, const SphereProjector& sp) const {
     std::vector<svg::Circle> result;
     for (const auto& [stop_name, stop] : stops) {
         svg::Circle symbol;
@@ -98,7 +103,7 @@ std::vector<svg::Circle> MapRenderer::GetStopsSymbols(std::map<std::string_view,
     return result;
 }
 
-std::vector<svg::Text> MapRenderer::GetStopsLabels(std::map<std::string_view, const transport_catalogue::Stop*> stops, const SphereProjector& sp) const {
+std::vector<svg::Text> MapRenderer::GetStopsLabels(std::map<std::string_view, const tc::Stop*> stops, const SphereProjector& sp) const {
     std::vector<svg::Text> result;
     svg::Text text;
     svg::Text underlayer;
@@ -107,14 +112,14 @@ std::vector<svg::Text> MapRenderer::GetStopsLabels(std::map<std::string_view, co
         text.SetOffset(render_settings_.stop_label_offset);
         text.SetFontSize(render_settings_.stop_label_font_size);
         text.SetFontFamily("Verdana");
-        text.SetData(stop->name);
+        text.SetData(stop->name_);
         text.SetFillColor("black");
         
         underlayer.SetPosition(sp(stop->coordinates_));
         underlayer.SetOffset(render_settings_.stop_label_offset);
         underlayer.SetFontSize(render_settings_.stop_label_font_size);
         underlayer.SetFontFamily("Verdana");
-        underlayer.SetData(stop->name);
+        underlayer.SetData(stop->name_);
         underlayer.SetFillColor(render_settings_.underlayer_color);
         underlayer.SetStrokeColor(render_settings_.underlayer_color);
         underlayer.SetStrokeWidth(render_settings_.underlayer_width);
@@ -128,18 +133,18 @@ std::vector<svg::Text> MapRenderer::GetStopsLabels(std::map<std::string_view, co
     return result;
 }
  
- svg::Document MapRenderer::GetSVG(const std::deque<transport_catalogue::Bus>& buses) const {
+ svg::Document MapRenderer::GetSVG(const std::deque<tc::Bus>& buses) const {
     svg::Document result;
     std::vector<geo::Coordinates> route_stops_coord;
-    std::map<std::string_view, const transport_catalogue::Stop*> all_stops;
+    std::map<std::string_view, const tc::Stop*> all_stops;
     
     for (const auto&  bus : buses) {
-        if (bus.stops.empty()) {
+        if (bus.stops_.empty()) {
             continue;
         }
-        for (const auto& stop : bus.stops) {
+        for (const auto& stop : bus.stops_) {
             route_stops_coord.push_back(stop->coordinates_);
-            all_stops[stop->name] = stop;
+            all_stops[stop->name_] = stop;
         }
     }
     
@@ -159,4 +164,5 @@ std::vector<svg::Text> MapRenderer::GetStopsLabels(std::map<std::string_view, co
  
     return result;
 }
+ 
 } // namespace renderer
