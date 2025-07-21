92f8090d7829b500d3297f460c81a62e41d70ddb
levyalinxzx <nufuha9@gmail.com>
2025-07-21 14:52:49 +0300

    Update domain.h

--- transport-catalogue/domain.h
+++ transport-catalogue/domain.h
@@ -4,26 +4,53 @@
 
 #include <string>
 #include <vector>
-#include <unordered_map>
+#include <set>
 
-namespace transport_catalogue{
+namespace tc{
 struct Stop
 {
-    std::string name;
-    geo::Coordinates coordinates_;
+    Stop() = default;
+
+    Stop(std::string name, geo::Coordinates coordinates);
+
+	std::string name_;
+	geo::Coordinates coordinates_;
 };
 
+using Route = std::vector<Stop*>;
 
 struct Bus {
-    std::string id;
-    std::vector<const Stop*> stops;	
-    bool is_circle;
+    Bus() = default;
+
+    Bus(std::string name_bus, Route stops, bool is_circle);
+
+    bool operator<(Bus& other) ;
+
+    std::string name_;
+    Route stops_;
+    bool is_circle_;	
 };
+
+struct BusPtrComparator {
+    bool operator() (Bus* rhs, Bus* lhs) const ;
+};
+
+using Buses = std::set<Bus*, BusPtrComparator>;
 
 struct BusInfo {
     size_t size;
     int unique_stops;
     int length_route;
     double curvature;
+};
+
+struct Hasher {
+    std::hash<const void*> hasher;
+    static const size_t salt = 24; 
+
+    size_t operator()(const Stop *stop) const;
+    
+    std::size_t operator()(const std::pair<const Stop*, const Stop*> pair_stops) const noexcept;
+    
 };
 }
