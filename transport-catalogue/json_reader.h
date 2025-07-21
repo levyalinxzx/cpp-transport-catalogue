c576690eeb1ddcc9a1e176b1a4e131267b146b75
levyalinxzx <nufuha9@gmail.com>
2025-07-21 14:56:58 +0300

    Update json_reader.h

--- transport-catalogue/json_reader.h
+++ transport-catalogue/json_reader.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#include "json.h"
+//#include "json.h"
+#include "json_builder.h"
 #include "transport_catalogue.h"
 #include "map_renderer.h"
 #include "request_handler.h"
@@ -16,13 +17,11 @@ public:
     const json::Node& GetBaseRequests() const;
     const json::Node& GetStatRequests() const;
     const json::Node& GetRenderSettings() const;
+    const json::Node& GetRoutingSettings() const;
 
-    void FillCatalogue(transport_catalogue::TransportCatalogue& catalogue);
+    void FillCatalogue(tc::TransportCatalogue& catalogue);
     renderer::MapRenderer FillRenderSettings(const json::Dict& request_map) const;
-
-    const json::Node PrintBus(const json::Dict& request_map, RequestHandler& rh) const;
-    const json::Node PrintStop(const json::Dict& request_map, RequestHandler& rh) const;
-    const json::Node PrintMap(const json::Dict& request_map, RequestHandler& rh) const;
+    tc::router::RoutingSettings FillRoutingSettings(const json::Node& settings) const;
 
     void ProcessRequests(const json::Node& stat_requests, RequestHandler& rh) const;
 
@@ -30,12 +29,19 @@ private:
     json::Document input_;
     json::Node dummy_ = nullptr;
 
-    const transport_catalogue::Stop FillStop(const json::Dict& request_map) const;
-    void FillStopDistances(transport_catalogue::TransportCatalogue& catalogue, const json::Array& stops_requests) const;
-    const transport_catalogue::Bus FillRoute(const json::Dict& request_map, transport_catalogue::TransportCatalogue& catalogue) const;
+    const tc::Stop FillStop(const json::Dict& request_map) const;
+    void FillStopDistances(tc::TransportCatalogue& catalogue, const json::Array& stops_requests) const;
+    const tc::Bus FillRoute(const json::Dict& request_map, tc::TransportCatalogue& catalogue) const;
     const std::tuple<json::Array, json::Array> SortedRequests(const json::Array& base_request) const;
-    void AddStops(const json::Array& stops_requests, transport_catalogue::TransportCatalogue& catalogue);
-    void AddBuses(const json::Array& buses_requests, transport_catalogue::TransportCatalogue& catalogue);
+    void AddStops(const json::Array& stops_requests, tc::TransportCatalogue& catalogue);
+    void AddBuses(const json::Array& buses_requests, tc::TransportCatalogue& catalogue);
     std::vector<svg::Color> ReadColors(const json::Array &json) const ;
     svg::Color ReadColor(const json::Node &json) const ;
+    void PrintBus(json::Builder& builder, const json::Dict& request_map, RequestHandler& rh) const;
+    void PrintStop(json::Builder& builder, const json::Dict& request_map, RequestHandler& rh) const;
+    void PrintMap(json::Builder& builder, RequestHandler& rh) const;
+    void PrintRoute(json::Builder& builder, const json::Dict& request_map, RequestHandler& rh) const;
+    void BuildRouteItem(json::Builder& builder, const tc::router::RouteInfo::WaitItem& item) const;
+    void BuildRouteItem(json::Builder& builder, const tc::router::RouteInfo::BusItem& item) const;
+    
 };
